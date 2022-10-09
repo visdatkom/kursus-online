d6a4fb20d256471d0dfecc06bb7d91bf75e0d032
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-10-09 23:36:04 +0800

    [add] tailwind css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.auth.app', ['title' => 'Homepage'])
+
+@section('content')
+    @include('layouts.frontend.partials.hero')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', App\Http\Controllers\HomeController::class);
 
 Route::group(['as' => 'admin.', 'prefix' => 'admin', 'middleware' => ['auth']], function(){
     Route::get('/dashboard', App\Http\Controllers\Admin\DashboardController::class)->name('dashboard');
