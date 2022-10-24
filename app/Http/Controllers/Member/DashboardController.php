85ed3733ec782c54ba79ce83cbc9a2089782255c
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-10-24 10:56:04 +0800

    [doc] MyCourseController

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -3,18 +3,14 @@
 namespace App\Http\Controllers\Member;
 
 use App\Models\Review;
-use App\Models\Category;
 use App\Models\Showcase;
-use App\Traits\HasCourse;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use App\Models\TransactionDetail;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    use HasCourse;
     /**
      * Handle the incoming request.
      *
@@ -23,20 +19,29 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // tampung data user yang sedang login kedalam variabel $user.
         $user = $request->user();
 
+        /*
+            tampung data transaction detail kedalam variabel $course, kemudian kita memanggil relasi menggunakan with,
+            selanjutnya kita melakukan sebuah query untuk mengambil data transaction yang memiliki status success dan seusai dengan user yang sedang login, selanjutnya kita jumlahkan data tersebut.
+        */
         $course = TransactionDetail::with('transaction', 'course.reviews')
                 ->whereHas('transaction', function($query) use($user){
                     $query->where('user_id', $user->id)->where('status', 'success');
                 })->count();
 
+        // tampung jumlah data review user yang sedang login kedalam variabel $review.
         $review = Review::where('user_id', $user->id)->count();
 
+        // tampung jumlah data transaction user yang sedang login dan memiliki status "success" kedalam variabel $transaction.
         $transaction = Transaction::where('user_id', $user->id)
             ->where('status', 'success')->count();
 
+        // tampung jumlah data showcase user yang sedang login kedalam variabel $showcase.
         $showcase = Showcase::where('user_id', $user->id)->count();
 
+        // passing variabel $course, $review, $transaction, dan $showcase kedalam view.
         return view('member.dashboard', compact('course', 'review', 'transaction', 'showcase'));
     }
 }
--- app/Http/Controllers/Member/MyCourseController.php
+++ app/Http/Controllers/Member/MyCourseController.php
@@ -17,15 +17,22 @@ class MyCourseController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // tampung data user yang sedang login kedalam variabel $user.
         $user = $request->user();
 
+        /*
+            tampung data transaction detail kedalam variabel $course, kemudian kita memanggil relasi menggunakan with,
+            selanjutnya kita melakukan sebuah query untuk mengambil data transaction yang memiliki status success dan seusai dengan user yang sedang login, selanjutnya kita juga melakukan query untuk pencarian data berdasarkan course name, kemudian kita pecah data transaction detail yang kita tampilkan hanya 3 per halaman
+            dengan urutan terbaru.
+        */
         $courses = TransactionDetail::with('transaction', 'course.reviews')
                 ->whereHas('transaction', function($query) use($user){
                     $query->where('user_id', $user->id)->where('status', 'success');
                 })->whereHas('course', function($query){
                     $query->where('name', 'like', '%'. request()->search .'%');
-                })->paginate(3);
+                })->latest()->paginate(3);
 
+        // passing variabel $courses kedalam view.
         return view('member.course.mycourse', compact('courses'));
     }
 }
